a636caf6132717c53dded4b365b189adc08d571f
Abdula <alkhilaevabdula@mail.ri>
2024-09-23 04:57:41 +0300

    added IconComponent, LoadingIndicator, search.svg and removed HelloWorld.vue

---
+++ src/components/IconComponent.vue
@@ -0,0 +1,33 @@
+<script>
+import { h } from "vue";
+import InlineSvg from "vue-inline-svg";
+
+export default {
+  name: "IconComponent",
+  props: {
+    name: String,
+  },
+  setup(props) {
+    let error = false;
+    let template = false;
+    if (!error) {
+      template = h(
+        "figure",
+        {
+          class: `icon ${props.name}-icon`,
+        },
+        h(InlineSvg, {
+          src: `/assets/svg/${props.name}.svg`,
+          onError: () => {
+            error = true;
+          },
+        })
+      );
+    }
+    return () => template;
+  },
+  data: () => ({
+    error: false,
+  }),
+};
+</script>
---
+++ src/components/LoadingIndicator.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="flex items-center gap-2">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="w-6 h-6"
+      viewBox="0 0 100 100"
+      preserveAspectRatio="xMidYMid"
+    >
+      <circle
+        cx="50"
+        cy="50"
+        fill="none"
+        stroke-width="8"
+        r="40"
+        stroke-dasharray="188.49555921538757 64.83185307179586"
+        class="stroke-gray-700"
+      >
+        <animateTransform
+          attributeName="transform"
+          type="rotate"
+          repeatCount="indefinite"
+          dur="0.6666666666666666s"
+          values="0 50 50;360 50 50"
+          keyTimes="0;1"
+        ></animateTransform>
+      </circle>
+    </svg>
+    <span v-if="title" class="text-gray-700">{{ title }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoadingIndicator",
+  props: {
+    title: {
+      type: String,
+      default: "Please wait, loading",
+    },
+  },
+  data() {
+    return {
+      dots: 0,
+      interval: null,
+    };
+  },
+  created() {
+    this.interval = setInterval(() => {
+      if (this.dots >= 3) {
+        this.dots = 0;
+      } else {
+        this.dots += 1;
+      }
+    }, 100);
+  },
+  beforeUnmount() {
+    clearInterval(this.interval);
+  },
+};
+</script>

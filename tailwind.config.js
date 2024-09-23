1f31af30e8e21120ce11d0718a6499ef1f21cd8a
Abdula <alkhilaevabdula@mail.ri>
2024-09-23 04:58:43 +0300

    changed body background-color and added styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      boxShadow: {
+        custom: "0 10px 60px 0 rgba(226, 236, 249, 0.5)",
+      },
+      borderRadius: {
+        "4xl": "30px",
+      },
+      colors: {
+        "white-100": "#FAFBFF",
+      },
       screens: {
-        'mobile': { 'max': '640px' },
-        'tablet': { 'min': '641px', 'max': '1024px' },
-        'desktop': { 'min': '1025px' },
+        mobile: { max: "640px" },
+        tablet: { min: "641px", max: "1024px" },
+        desktop: { min: "1025px" },
       },
     },
   },
   plugins: [],
-}
+};
